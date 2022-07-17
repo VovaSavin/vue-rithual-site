1e33b807bf93cc5787b01faa7001c0d8291c8bf4
Uvecon <savin@uvecon.ua>
2022-07-17 13:21:25 +0300

    Created all views

---
+++ src/assets/data.js
@@ -0,0 +1,14 @@
+function navigator() {
+    let temp = [
+        { value: 0, text: "Головна", link: "home" },
+        { value: 1, text: "Ритуальні послуги", link: "Services" },
+        { value: 2, text: "Ритуальні товари", link: "Goods" },
+        { value: 3, text: "Про нас", link: "about" },
+    ];
+    return temp;
+}
+
+
+export {
+    navigator,
+}
---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    backendUrl: "http://127.0.0.1:8000/api/v1",
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  },
+  getters: {
+    getServerUrl: state => {
+      return state.backendUrl
+    },
+  }
+})
