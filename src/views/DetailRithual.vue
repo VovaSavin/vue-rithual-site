271d16f5a0b0aa294f951f34dfc348633a584dc7
VovaSavin <volodimirsavin56@gmail.com>
2022-10-14 13:53:15 +0300

    Nothing

--- src/views/DetailRithual.vue
+++ src/views/DetailRithual.vue
@@ -90,7 +90,7 @@ export default {
     },
     chooseData() {
       // Choose getter detatil data for goods or service
-      window.scrollTo(0, 500);
+      window.scrollTo(0, 650);
       if (this.dataDetail) {
         return this.dataDetail;
       } else {
